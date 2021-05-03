fcb5c22244d923b1101be6749ea7713d1148e3c6
Sarthak-777 <tn.sarthak039@gmail.com>
2021-05-03 17:57:33 +0545

    Connecting and adding users to Mongo

--- mongodb.js
+++ mongodb.js
@@ -5,6 +5,19 @@ const mongodb = require('mongodb');
 //Required for connection to the database
 const MongoClient = mongodb.MongoClient;
 
-const connectionUrl = `mongoDb://127.0.0.1:27017`;
-const databaseName= `task-manager`;
+const connectionUrl = 'mongodb://127.0.0.1:27017';
+const databaseName= 'task-manager';
+
+MongoClient.connect(connectionUrl, { useNewUrlParser: true }, (error, client) => {
+    if (error){
+        return console.log('Unable to connect to the database');
+    }
+    console.log('Connected correctly');
+    const db = client.db(databaseName);
+
+    db.collection("users").insertOne({
+        name: "Sarthak",
+        age: 19
+    })
+})
 
