dc4e0fdda65536c98d26475290dece08df25b3fc
Sarthak-777 <tn.sarthak039@gmail.com>
2021-05-05 13:50:57 +0545

    id

--- mongodb.js
+++ mongodb.js
@@ -1,12 +1,19 @@
 //CRUD create read update delete
 
-const mongodb = require('mongodb');
+// const mongodb = require('mongodb');
 
-//Required for connection to the database
-const MongoClient = mongodb.MongoClient;
+// //Required for connection to the database
+// const MongoClient = mongodb.MongoClient;
+// const ObjectID = mongodb.ObjectID;
+
+const {MongoClient, ObjectId, ObjectID} = require('mongodb');
 
 const connectionUrl = 'mongodb://127.0.0.1:27017';
 const databaseName= 'task-manager';
+
+const id = new ObjectID();
+console.log(id);
+console.log(id.getTimestamp());
 
 MongoClient.connect(connectionUrl, { useNewUrlParser: true }, (error, client) => {
     if (error){
@@ -15,9 +22,9 @@ MongoClient.connect(connectionUrl, { useNewUrlParser: true }, (error, client) =>
     console.log('Connected correctly');
     const db = client.db(databaseName);
 
-    db.collection("users").insertOne({
-        name: "Sarthak",
-        age: 19
-    })
+//     db.collection("users").insertOne({
+//         name: "Sarthak",
+//         age: 19
+//     })
 })
 
