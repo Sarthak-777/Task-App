8a843242d6fcc311f06f3fb563ac0aa853bb7443
Sarthak-777 <tn.sarthak039@gmail.com>
2021-05-05 14:04:07 +0545

    query init

--- mongodb.js
+++ mongodb.js
@@ -11,10 +11,6 @@ const {MongoClient, ObjectId, ObjectID} = require('mongodb');
 const connectionUrl = 'mongodb://127.0.0.1:27017';
 const databaseName= 'task-manager';
 
-const id = new ObjectID();
-console.log(id);
-console.log(id.getTimestamp());
-
 MongoClient.connect(connectionUrl, { useNewUrlParser: true }, (error, client) => {
     if (error){
         return console.log('Unable to connect to the database');
@@ -22,15 +18,5 @@ MongoClient.connect(connectionUrl, { useNewUrlParser: true }, (error, client) =>
     console.log('Connected correctly');
     const db = client.db(databaseName);
 
-    db.collection("users").insertOne({
-        _id: id,
-        name: "Sarthak",
-        age: 19
-     }, (error, result) => {
-        if (error){
-            return console.log('Unable to insert Docs');
-        }
-        console.log(result.ops)
-    })
 })
 
